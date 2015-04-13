75c608afe947b2638441c1a4319f9fbb73d84d45
Max Gurewitz <mgurewitz@MacBook-Pro.local>
2015-04-13 14:01:35 -0700

    shell script for initializing plugins:

---
+++ init.sh
@@ -0,0 +1,14 @@
+cd .vim/bundle
+rm -rf ./*
+git clone git@github.com:mileszs/ack.vim.git
+git clone git@github.com:kien/ctrlp.vim.git
+git clone git@github.com:scrooloose/nerdtree.git
+git clone git@github.com:rust-lang/rust.vim.git
+git clone git@github.com:kchmck/vim-coffee-script.git
+git clone git@github.com:altercation/vim-colors-solarized.git
+git clone git@github.com:tpope/vim-commentary.git
+git clone git@github.com:tpope/vim-fugitive.git
+git clone git@github.com:digitaltoad/vim-jade.git
+git clone git@github.com:mustache/vim-mustache-handlebars.git
+git clone git@github.com:cespare/vim-toml.git
+cd ../..
