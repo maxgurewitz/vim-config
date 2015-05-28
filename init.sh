7b493092fb9cab3e54ad4bbe5d5353a0a10d87b0
Max Gurewitz <mgurewitz@MacBook-Pro.local>
2015-05-27 17:07:42 -0700

    include elm

--- init.sh
+++ init.sh
@@ -11,4 +11,5 @@ git clone git@github.com:tpope/vim-fugitive.git
 git clone git@github.com:digitaltoad/vim-jade.git
 git clone git@github.com:mustache/vim-mustache-handlebars.git
 git clone git@github.com:cespare/vim-toml.git
+git clone git@github.com:lambdatoast/elm.vim.git
 cd ../..
